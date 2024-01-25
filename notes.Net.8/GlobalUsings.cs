8d38a748d0f69df26256d32f0ceec566ce3c1a5d
Carlos Gomez <67838217+CarlosNetAcad@users.noreply.github.com>
2024-01-24 23:21:19 -0500

    Create GlobalUsings.cs

---
+++ notes.Net.8/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using notes.Net8.Domain.Models;
+global using notes.Net8.Presentation.ViewModels;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Runtime.CompilerServices;
+global using System.Text.Json;
