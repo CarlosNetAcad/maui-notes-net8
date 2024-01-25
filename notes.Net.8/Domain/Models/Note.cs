cca6425dc468449efda5f7f0079236c0532df00c
Carlos Gomez <67838217+CarlosNetAcad@users.noreply.github.com>
2024-01-24 22:44:53 -0500

    Update Note.cs
    Create SQLite Entity.

--- notes.Net.8/Domain/Models/Note.cs
+++ notes.Net.8/Domain/Models/Note.cs
@@ -1,11 +1,20 @@
 ﻿using System;
-namespace notes.Net._8.Domain.Models
+using SQLite;
+
+namespace notes.Net8.Domain.Models
 {
 	public class Note
 	{
-		public Note()
-		{
-		}
+		[PrimaryKey]
+        [AutoIncrement]
+        public long ID              { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime LastUpdated { get; set; } = DateTime.Now;
+        public string? Title        { get; set; }
+        public string? Description  { get; set; }
+        public long UserId          { get; set; }
+        public double Latitude      { get; set; }
+        public double Longitude     { get; set; }
 	}
 }
 
