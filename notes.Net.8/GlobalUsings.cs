b6eccfe3e4eb237053b259adac76d325e321aa06
Carlos Gomez <67838217+CarlosNetAcad@users.noreply.github.com>
2024-01-31 00:51:32 -0500

    [UPDATE] Register and resolve dependencies.

--- notes.Net.8/GlobalUsings.cs
+++ notes.Net.8/GlobalUsings.cs
@@ -1,9 +1,11 @@
 ﻿global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Input;
-global using notes.Net8.Domain.Models;
-global using notes.Net8.Presentation.ViewModels;
 global using System.Collections.ObjectModel;
 global using System.ComponentModel;
 global using System.Diagnostics;
 global using System.Runtime.CompilerServices;
 global using System.Text.Json;
+
+global using notes.Net8.Shared.Domain.Constants;
+global using notes.Net8.Shared.Presentation.ViewModels;
+global using notes.Net8.Shared.Infrastructure;
--- notes.Net.8/MauiProgram.cs
+++ notes.Net.8/MauiProgram.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
+using notes.Net8.Notes.Infrastructure.Interfaces;
+using notes.Net8.Notes.Infrastructure.Services;
+using notes.Net8.Notes.Presentation.ViewModels;
 
 namespace notes.Net._8;
 
@@ -18,8 +21,26 @@ public static class MauiProgram
 #if DEBUG
 		builder.Logging.AddDebug();
 #endif
+		Bootstrap( builder );
 
 		return builder.Build();
+	}
+
+	static void Bootstrap( MauiAppBuilder builder)
+	{
+		//-> Main pages
+
+		//->Essentials
+
+		//->Notes
+		builder.Services.AddSingleton<INoteService>(b => new NoteService());
+		builder.Services.AddTransient<NotesViewModel>(
+			b => new NotesViewModel(
+				title: PageTitles.NOTES_DASHBOARD,
+				b.GetRequiredService<INoteService>()
+			)
+		);
+
 	}
 }
 
